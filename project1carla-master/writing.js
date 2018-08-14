4ffa3e2474e776a9508fb25a9f5b2a364d187939
anjali <anjali99malu@gmail.com>
2018-08-13 19:19:25 -0700

    carlas project

---
+++ project1carla-master/writing.js
@@ -0,0 +1,89 @@
+var word1="";
+			var word2="";
+			var word3="";
+			var word4="";
+			var result1="No Result";
+			var result2="No Result";
+			var result3="No Result";
+			var result4="No Result";
+			var result5="No Result";
+			
+			function enteranswer1(){
+				word1=prompt("Enter the answer");
+				if(word1=="avocado"){
+					var result1="You are right!!";
+					
+				}
+				else {
+					var result1="Sorry, that's incorrect";
+				}
+				alert("Click Check Answer to see the result");
+			}
+			
+			function showresult1(){
+				alert(result1);
+			}
+			
+			function enteranswer2(){
+				word2=prompt("Enter the answer");
+				if(word2=="banana"){
+					var result2="You are right!!";
+					
+				}
+				else {
+					var result2="Sorry, that's incorrect";
+				}
+				alert("Click Check Answer to see the result");
+			}
+			
+			function showresult2(){
+				alert(result2);
+			}
+			
+			function enteranswer3(){
+				word3=prompt("Enter the answer");
+				if(word3=="celery"){
+					var result3="You are right!!";
+					
+				}
+				else {
+					var result3="Sorry, that's incorrect";
+				}
+				alert("Click Check Answer to see the result");
+			}
+			
+			function showresult3(){
+				alert(result3);
+			}
+			
+			function enteranswer4(){
+				word4=prompt("Enter the answer");
+				if(word4=="lemonade"){
+					var result4="You are right!!";
+					
+				}
+				else {
+					var result4="Sorry, that's incorrect";
+				}
+				alert("Click Check Answer to see the result");
+			}
+			
+			function showresult4(){
+				alert(result4);
+			}
+			
+			function enteranswer5(){
+				word5=prompt("Enter the answer");
+				if(word5=="onion"){
+					var result5="You are right!!";
+					
+				}
+				else {
+					var result5="Sorry, that's incorrect";
+				}
+				alert("Click Check Answer to see the result");
+			}
+			
+			function showresult5(){
+				alert(result5);
+			}
